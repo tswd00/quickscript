52dca693237dbe948c55b2083476143e77147a7d
tswd00 <tswd00@gmail.com>
2016-02-05 22:30:57 -0500

    Add qsSplit() within closure in /src/splitter.js

--- src/splitter.js
+++ src/splitter.js
@@ -4,3 +4,18 @@
  * This file contains part of the code for the QS parser. This file splits the code into "parse units."
  */
 
+// Call an anonymous function with window inputted as context so that variables are not stored in window by default
+(function(context) {
+  
+  // qsSplit() - takes code (converted to string) and splits it into "parse untis"
+  var qsSplit = function(code) {
+    code += ''; // Convert code to a string
+    
+    var originalCode = code; // Store the original code in case needed later
+    
+    code = code.split(/([^a-z]+)/gi); // Split the code by characters other than a-z
+    
+    return code;
+  };
+  
+})(window);
